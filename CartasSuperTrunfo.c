aef356df726624477ab5118281005c682ebe0b08
Ade-hrq <Adeilsonhrc@gmail.com>
2025-02-05 16:47:06 +0000

    Calculos de pib e população

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,30 +1,50 @@
-
+#include <stdio.h>
+#include <locale.h>
 
 int main() {
+
+    setlocale(LC_ALL,"Portuguese_Brazil");
 
     int turismo, pessoas, codigo, pib;
     float area;
     char nome[50];
 
-    printf("Nome da cidade: ");
+    printf("** Super trunfo paÌses **\n");
+
+// Insere as informaÁıes da carta 
+    printf("\nNome da cidade: ");
     scanf("%s", &nome);
 
-    printf("Popula√ß√£o: ");
+    printf("PopulaÁ„o: ");
     scanf("%d", &pessoas);
 
     printf("Quantidade de locais turisticos: ");
     scanf("%d", &turismo);
 
-    printf("√Årea da cidade: ");
+    printf("¡rea da cidade: ");
     scanf("%f", &area);
 
-    printf("C√≥digo da cidade: ");
+    printf("CÛdigo da cidade: ");
     scanf("%d", &codigo);
 
     printf("PIB: ");
     scanf("%d", &pib);
 
-    printf("\nNome: %s\n Popula√ß√£o: %d\n Locais turisticos: %d\n √Årea: %f\n", nome, pessoas, turismo, area);
-    printf("C√≥digo: %d\n PIB: %d", codigo, pib);
+    printf("\n** Valores da sua carta **\n");
+// Exibe os valores inseridos anteriormente
+    printf("\nNome: %s\n PopulaÁ„o: %d\n Locais turisticos: %d\n ¡rea: %.3fm≤\n", nome, pessoas, turismo, area);
+    printf(" CÛdigo: %d\n PIB: %d\n", codigo, pib);
 
-}
+    printf("\n** Resultados **\n");
+
+// Calcula o PIB e a Densidade populacionacional e a exibe tambÈm
+    int pibfinal = pessoas / pib ;
+    int densidade = pessoas / area;
+
+    printf("\n- Densidade populacional: %d\n", densidade);
+    printf("\n- PIB per capita: %d\n", pibfinal);
+
+
+    return 0;
+    
+    }
