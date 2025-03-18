93c3254f80e3510d7263ac5b14dfaf800f63b459
Ade-hrq <Adeilsonhrc@gmail.com>
2025-03-18 21:58:14 +0000

    Super Trunfo cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
-#include <locale.h>
+
 
 int main() {
-
-    setlocale(LC_ALL,"Portuguese_Brazil");
 
     int turismo, pessoas, codigo, pib;
     float area;
     char nome[50];
 
-    printf("** Super trunfo países **\n");
+    printf("** Super trunfo paises **\n");
+    printf("\n**Carta 01**\n");
 
-// Insere as informações da carta 
+// Insere as informacoes da carta 
     printf("\nNome da cidade: ");
     scanf("%s", &nome);
 
-    printf("População: ");
+    printf("Populacao: ");
     scanf("%d", &pessoas);
 
     printf("Quantidade de locais turisticos: ");
     scanf("%d", &turismo);
 
-    printf("Área da cidade: ");
+    printf("Area da cidade: ");
     scanf("%f", &area);
 
-    printf("Código da cidade: ");
+    printf("Codigo da cidade: ");
     scanf("%d", &codigo);
 
     printf("PIB: ");
@@ -32,18 +31,56 @@ int main() {
 
     printf("\n** Valores da sua carta **\n");
 // Exibe os valores inseridos anteriormente
-    printf("\nNome: %s\n População: %d\n Locais turisticos: %d\n Área: %.3fm²\n", nome, pessoas, turismo, area);
-    printf(" Código: %d\n PIB: %d\n", codigo, pib);
+    printf("\nNome: %s\n Populacao: %d\n Locais turisticos: %d\n Area: %.3fm2\n", nome, pessoas, turismo, area);
+    printf(" Codigo: %d\n PIB: %d\n", codigo, pib);
 
-    printf("\n** Resultados **\n");
+    printf("\n** Resultado carta 01**\n");
 
-// Calcula o PIB e a Densidade populacionacional e a exibe também
+// Calcula o PIB e a Densidade populacionacional e a exibe tambï¿½m
     int pibfinal = pessoas / pib ;
     int densidade = pessoas / area;
 
     printf("\n- Densidade populacional: %d\n", densidade);
     printf("\n- PIB per capita: %d\n", pibfinal);
 
+    printf("\n**Carta 02**\n");
+
+    int turismo2, pessoas2, codigo2, pib2;
+    float area2;
+    char nome2[50];
+
+// Insere as informacoes da carta 
+    printf("\nNome da cidade: ");
+    scanf("%s", &nome2);
+
+    printf("Populacao: ");
+    scanf("%d", &pessoas2);
+
+    printf("Quantidade de locais turisticos: ");
+    scanf("%d", &turismo2);
+
+    printf("Area da cidade: ");
+    scanf("%f", &area2);
+
+    printf("Codigo da cidade: ");
+    scanf("%d", &codigo2);
+
+    printf("PIB: ");
+    scanf("%d", &pib2);
+
+    printf("\n** Valores da sua carta **\n");
+// Exibe os valores inseridos anteriormente
+    printf("\nNome: %s\n Populacao: %d\n Locais turisticos: %d\n Area: %.3fm2\n", nome2, pessoas2, turismo2, area2);
+    printf(" Codigo: %d\n PIB: %d\n", codigo2, pib2);
+
+    printf("\n** Resultado carta 02 **\n");
+
+// Calcula o PIB e a Densidade populacionacional e a exibe tambï¿½m
+    int pibfinal2 = pessoas2 / pib2 ;
+    int densidade2 = pessoas2 / area2;
+
+    printf("\n- Densidade populacional: %d\n", densidade2);
+    printf("\n- PIB per capita: %d\n", pibfinal2);
 
     return 0;
     
